1599f8131b67e32c5d7519b19ae5ec249b3e13ca
James Lale <fh231@OA.PNRAD.NET>
2017-11-08 21:55:59 +0100

    assignment1

---
+++ assignment1/VehicleService/VehicleDBApplication/Vehicle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VehicleDBApplication
+{
+    abstract class Vehicle
+    {
+        private string numberPlate;
+        private int mileage;
+        private double rentalCharge;
+
+        public string NumberPlate
+        {
+            get { return numberPlate; }
+            set { numberPlate = value; }
+        }
+
+
+        public int Mileage
+        {
+            get { return mileage; }
+            set { mileage = value; }
+        }
+        
+
+        public double RentalCharge
+        {
+            get { return rentalCharge; }
+            set { rentalCharge = value; }
+        }
+    }
+
+    class Vehicle4WD : Vehicle
+    {
+        private bool offRoad = false;
+        private bool dirtRoad = true;
+        private bool normalRoad = true;
+
+        public bool OffRoad
+        {
+            get { return offRoad; }
+            set { offRoad = value; }
+        }
+        
+
+        public bool DirtRoad
+        {
+            get { return dirtRoad; }
+            set { dirtRoad = value; }
+        }
+        
+
+        public bool NormalRoad
+        {
+            get { return normalRoad; }
+            set { normalRoad = value; }
+        }
+
+    }
+
+    class Vehicle2WD : Vehicle
+    {
+        private bool under21 = false;
+
+        public bool Under21
+        {
+            get { return under21; }
+            set { under21 = value; }
+        }
+    }
+
+    class CamperVan : Vehicle
+    {
+        private int numberBeds;
+        private bool toilet;
+
+        public int NumberBeds
+        {
+            get { return numberBeds; }
+            set { numberBeds = value; }
+        }
+
+        public bool Toilet
+        {
+            get { return toilet; }
+            set { toilet = value; }
+        }
+
+
+    }
+}
---
+++ assignment1/VehicleService/VehicleService/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace VehicleService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class VehicleService : IVehicleService
+    {
+        /*
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+         */
+
+        public string search(string numberPlate)
+        {
+            //fetch from database
+            return string.Format("You entered: {0}", numberPlate);
+        }
+
+        public void create()
+        {
+            //create new vehicle
+            //insert into db
+        }
+
+        public string list()
+        {
+            //fetch all vehicles from db
+            return "vehicles list";
+        }
+
+
+    }
+}
