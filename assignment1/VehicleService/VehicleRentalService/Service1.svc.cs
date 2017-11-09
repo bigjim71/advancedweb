b37ac7cd9a2e11d48321e0b0e2563276be12d2b9
James Lale <fh231@OA.PNRAD.NET>
2017-11-09 20:27:15 +0100

    assignment1 consoleapp

---
+++ assignment1/VehicleService/VehicleRentalService/Service1.svc.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using VehicleDBApplication;
+using DTOClassLibrary;
+using VehicleRentalService.ServiceReferenceUpper;
+
+namespace VehicleRentalService
+{
+    public class Service1 : IService1
+    {
+
+        public List<SoapVehicle> search(string numberPlate)
+        {
+
+
+            /*
+             * For some reason this isn't working.
+             * It should work, as I've done the same as in the labs where it did work.
+             * I've had to re-create the VehicleRentalService several times as VS screwed up some config files (impossible to figure what was wrong).
+             * Project wouldn't even compile.
+             * Now, at least, it compiles, but there is still the runtime error.
+             * Too often the server says it's "Too busy"! I don't know why, there is only one request ??? 
+             * Constantly have to restart the IDE to clear the IIS successfully.
+             * 
+             * Exception is:
+             * There was no endpoint listening at https://campus.cs.le.ac.uk/tyche/axis2/services/UpperOperator that could accept the message. 
+             * This is often caused by an incorrect address or SOAP action. See InnerException, if present, for more details.
+             */
+            //ServiceReferenceUpper.UpperOperatorPortTypeClient upperSvc = new ServiceReferenceUpper.UpperOperatorPortTypeClient("UpperOperatorSOAP12port_http");
+            //string plate = upperSvc.upper(numberPlate);
+
+            //fetch from database
+            VehicleDBApplication.DBService dbService = new DBService();
+            List<Vehicle> vList = dbService.advancedSearch(numberPlate);
+            List<SoapVehicle> soapVehicles = new List<SoapVehicle>();
+
+            foreach (Vehicle v in vList)
+            {
+                SoapVehicle sv = new SoapVehicle();
+                sv.NumberPlate = v.NumberPlate;
+                sv.VehicleType = v.VehicleType;
+                sv.Mileage = v.Mileage;
+                sv.RentalCharge = v.RentalCharge;
+                soapVehicles.Add(sv);
+
+            }
+            return soapVehicles;
+        }
+
+        public SoapVehicle create(string numberPlate)
+        {
+
+            /*
+             * For some reason this isn't working.
+             * It should work, as I've done the same as in the labs where it did work.
+             * I've had to re-create the VehicleRentalService several times as VS screwed up some config files (impossible to figure what was wrong).
+             * Project wouldn't even compile.
+             * Now, at least, it compiles, but there is still the runtime error.
+             * Too often the server says it's "Too busy"! I don't know why, there is only one request ??? 
+             * Constantly have to restart the IDE to clear the IIS successfully.
+             * 
+             * Exception is:
+             * There was no endpoint listening at https://campus.cs.le.ac.uk/tyche/axis2/services/UpperOperator that could accept the message. 
+             * This is often caused by an incorrect address or SOAP action. See InnerException, if present, for more details.
+             */
+            //ServiceReferenceUpper.UpperOperatorPortTypeClient upperSvc = new ServiceReferenceUpper.UpperOperatorPortTypeClient("UpperOperatorSOAP12port_http");
+            //string plate = upperSvc.upper(numberPlate);
+
+            //create new vehicle
+            VehicleDBApplication.DBService dbService = new DBService();
+
+            DTOClassLibrary.Vehicle2WD moto = new Vehicle2WD();
+            moto.NumberPlate = numberPlate;
+            moto.VehicleType = "2WD";
+            moto.Mileage = 10000;
+            moto.RentalCharge = 10.46;
+            moto.Under21 = true;
+
+            dbService.createVehicle(moto);
+            SoapVehicle sv = new SoapVehicle();
+            sv.NumberPlate = moto.NumberPlate;
+            sv.VehicleType = moto.VehicleType;
+            sv.Mileage = moto.Mileage;
+            sv.RentalCharge = moto.RentalCharge;
+
+            return sv;
+        }
+
+        public List<SoapVehicle> list()
+        {
+            //fetch all vehicles from db
+            VehicleDBApplication.DBService dbService = new DBService();
+            List<Vehicle> vList = dbService.listVehicles();
+            List<SoapVehicle> soapVehicles = new List<SoapVehicle>();
+
+            foreach (Vehicle v in vList)
+            {
+                SoapVehicle sv = new SoapVehicle();
+                sv.NumberPlate = v.NumberPlate;
+                sv.VehicleType = v.VehicleType;
+                sv.Mileage = v.Mileage;
+                sv.RentalCharge = v.RentalCharge;
+                soapVehicles.Add(sv);
+
+            }
+            return soapVehicles;
+        }
+    }
+}
---
+++ assignment1/VehicleService/VehicleService/IVehicleService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace VehicleService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IVehicleService
+    {
+        /*
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);*/
+
+        [OperationContract]
+        List<SoapVehicle> search(string numberPlate);
+
+        [OperationContract]
+        SoapVehicle create(string numberPlate);
+
+        [OperationContract]
+        List<SoapVehicle> list();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class SoapVehicle
+    {
+        private string numberPlate;
+        private int mileage;
+        private double rentalCharge;
+        private string vehicleType;
+
+        [DataMember]
+        public string NumberPlate
+        {
+            get { return numberPlate; }
+            set { numberPlate = value; }
+        }
+
+        [DataMember]
+        public int Mileage
+        {
+            get { return mileage; }
+            set { mileage = value; }
+        }
+
+        [DataMember]
+        public double RentalCharge
+        {
+            get { return rentalCharge; }
+            set { rentalCharge = value; }
+        }
+
+        [DataMember]
+        public string VehicleType
+        {
+            get { return vehicleType; }
+            set { vehicleType = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle: " + numberPlate + " of type: " + vehicleType;
+        }
+
+    }
+}
